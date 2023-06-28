8e8ee114ba601b0d722e5d97b870b6823d0ee9f7
gustavocadev <gustavocadev@gmail.com>
2023-06-27 23:17:15 -0500

    fix: add correct route

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,5 +1,5 @@
 import { Slot, component$ } from '@builder.io/qwik';
-import { ProductsProvider } from '~/context/product/productsProvider';
+import { ProductsProvider } from '~/context/product/ProductsProvider';
 
 export default component$(() => {
   return (
