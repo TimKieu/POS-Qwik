89e874a4af9b3164964c83f6c781377cb1bc82ba
gustavocadev <gustavocadev@gmail.com>
2023-06-27 22:15:31 -0500

    chore: rm unused methods

--- src/context/product/ProductsContext.ts
+++ src/context/product/ProductsContext.ts
@@ -1,6 +1,4 @@
-import { createContextId, type QRL } from '@builder.io/qwik';
-import type { DocumentData, DocumentReference } from 'firebase/firestore';
-import type { Product } from '~/types/Product';
+import { createContextId } from '@builder.io/qwik';
 
 type Category = {
   id: number;
@@ -8,18 +6,7 @@ type Category = {
 };
 export type ProductContext = {
   categories: Category[];
-  products: Product[];
   isProductsEmpty: boolean;
-
-  // methods
-  getProducts: QRL<() => Promise<Product[]>>;
-  createProduct: QRL<(product: Product) => Promise<void>>;
-  updateProduct: QRL<
-    (
-      docRef: DocumentReference<DocumentData>,
-      product: Omit<Product, 'id'>
-    ) => Promise<void>
-  >;
 };
 
 export const ProductsContext = createContextId<ProductContext>('products');
