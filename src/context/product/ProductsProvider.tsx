b202239b40fff99470913e6cbb3bd9cb8678b277
gustavocadev <gustavocadev@gmail.com>
2023-06-27 22:16:08 -0500

    refactor: rm unused methods

--- src/context/product/ProductsProvider.tsx
+++ src/context/product/ProductsProvider.tsx
@@ -1,23 +1,11 @@
 import {
-  $,
   Slot,
   component$,
   useContextProvider,
   useStore,
-  useTask$,
 } from '@builder.io/qwik';
-import { getCollection, getDB } from '~/config/firebase';
-import {
-  type DocumentData,
-  type DocumentReference,
-  addDoc,
-  collection,
-  orderBy,
-  query,
-  updateDoc,
-} from 'firebase/firestore';
-import { ProductContext, ProductsContext } from './ProductsContext';
-import type { Product } from '~/types/Product';
+
+import { type ProductContext, ProductsContext } from './ProductsContext';
 
 export const ProductsProvider = component$(() => {
   const categories = [
@@ -35,61 +23,9 @@ export const ProductsProvider = component$(() => {
     },
   ];
 
-  const getProducts = $(async () => {
-    const collectionRef = collection(await getDB(), 'product');
-    const q = query(collectionRef, orderBy('availability', 'asc'));
-    return await getCollection(q);
-  });
-
-  // const db = useFirestore();
-
-  const createProduct = $(async (product: Product) => {
-    const collectionRef = collection(await getDB(), 'product');
-
-    // change to: useFirestore() -> db
-    await addDoc(collectionRef, product);
-  });
-
-  const updateProduct = $(
-    async (
-      docRef: DocumentReference<DocumentData>,
-      product: Omit<Product, 'id'>
-    ) => {
-      const { image, ...values } = product;
-      if (image !== '') {
-        console.log({
-          msg: 'Imagen actualizada con exito',
-        });
-        await updateDoc(docRef, {
-          ...values,
-          image,
-        });
-        return;
-      }
-      console.log({
-        msg: 'Imagen no actualizada',
-      });
-      await updateDoc(docRef, values);
-    }
-  );
-
   const store = useStore<ProductContext>({
     categories,
-    products: [] as Product[],
     isProductsEmpty: false,
-
-    // methods
-    createProduct,
-    updateProduct,
-    getProducts,
-  });
-
-  useTask$(async ({ track }) => {
-    track(() => [store.products]);
-
-    store.products = await getProducts();
-
-    if (store.products.length === 0) store.isProductsEmpty = true;
   });
 
   useContextProvider(ProductsContext, store);
