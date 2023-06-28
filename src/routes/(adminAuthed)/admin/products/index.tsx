56d005be5fca85235c294c70e2050d1b6a47a81a
gustavocadev <gustavocadev@gmail.com>
2023-06-27 22:14:30 -0500

    chore: load products

--- src/routes/(adminAuthed)/admin/products/index.tsx
+++ src/routes/(adminAuthed)/admin/products/index.tsx
@@ -1,11 +1,16 @@
 import { component$, useContext } from '@builder.io/qwik';
-import { Link } from '@builder.io/qwik-city';
+import { Link, routeLoader$ } from '@builder.io/qwik-city';
 import { Product } from '~/components/product/Product';
 import { ProductsContext } from '~/context/product/ProductsContext';
+import { getProducts } from '~/helpers/products';
+
+export const useProducts = routeLoader$(() => {
+  return getProducts();
+});
 
 export default component$(() => {
-  const { products, isProductsEmpty } = useContext(ProductsContext);
-
+  const { isProductsEmpty } = useContext(ProductsContext);
+  const products = useProducts();
   return (
     <>
       <Link
@@ -25,7 +30,7 @@ export default component$(() => {
         role="list"
         class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5"
       >
-        {products.map((product) => {
+        {products.value.map((product) => {
           return <Product key={product.image} product={product} />;
         })}
       </ul>
