7d93dca9cb95a6fa73acd4bae009a919b22c58fe
gustavocadev <gustavocadev@gmail.com>
2023-06-29 10:22:02 -0500

    chore: rename dir

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,9 +1,3 @@
-import {
-  useStore,
-  type NoSerialize,
-  useVisibleTask$,
-  noSerialize,
-} from '@builder.io/qwik';
 import { initializeApp } from 'firebase/app';
 import {
   getFirestore,
@@ -13,36 +7,21 @@ import {
   type DocumentReference,
   getDoc,
 } from 'firebase/firestore';
-import { type FirebaseStorage, getStorage } from 'firebase/storage';
+import { getStorage } from 'firebase/storage';
 import type { Product } from '~/types/Product';
 import { $ } from '@builder.io/qwik';
 import type { Product as IProduct } from '~/types/Product';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 export const firebaseApp = initializeApp(firebaseConfig);
-
-// this  returns a storage
-export const useFireBaseStorage = () => {
-  const store = useStore<{ storageInstance: NoSerialize<FirebaseStorage> }>({
-    storageInstance: undefined,
-  });
-
-  useVisibleTask$(() => {
-    // This works only in the browser
-    const storage = getStorage(firebaseApp);
-    store.storageInstance = noSerialize(storage);
-  });
-
-  return store.storageInstance;
-};
 
 export const getFirebaseStorage = $(() => getStorage(firebaseApp));
 
