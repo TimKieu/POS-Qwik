d8fcaa66be4b5500bc19e36dc6ebeaf0c45bd6c6
gustavocadev <gustavocadev@gmail.com>
2023-07-01 21:12:13 -0500

    chore: create new type for store

--- src/context/product/ProductsContext.ts
+++ src/context/product/ProductsContext.ts
@@ -1,12 +1,18 @@
-import { createContextId } from '@builder.io/qwik';
+import { type Signal, createContextId } from '@builder.io/qwik';
 
 type Category = {
   id: number;
   name: string;
 };
-export type ProductContext = {
+
+export type ProductsStore = {
   categories: Category[];
   isProductsEmpty: boolean;
+};
+
+export type ProductContext = {
+  productsStore: ProductsStore;
+  categorySelected: Signal<number>;
 };
 
 export const ProductsContext = createContextId<ProductContext>('products');
