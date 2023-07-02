da86d16db234b7c08962c39ce587bd7050f59e0a
gustavocadev <gustavocadev@gmail.com>
2023-07-01 21:08:58 -0500

    chore: load products

--- src/routes/(mainAuthed)/index.tsx
+++ src/routes/(mainAuthed)/index.tsx
@@ -1,18 +1,50 @@
-import { component$, useComputed$, useSignal } from '@builder.io/qwik';
+import {
+  component$,
+  useComputed$,
+  useContext,
+  useVisibleTask$,
+} from '@builder.io/qwik';
+import { routeLoader$ } from '@builder.io/qwik-city';
+import { ProductCard } from '~/components/product/ProductCard';
+import { ProductsContext } from '~/context/product/ProductsContext';
+import { getProductsByCategory } from '~/helpers/products';
+
+export const useProductsLoader = routeLoader$(({ query }) => {
+  const categoryId = query.get('category');
+
+  console.log({
+    categoryId,
+  });
+  // by default, show products from category 1
+  if (!categoryId) return getProductsByCategory(2);
+
+  return getProductsByCategory(Number(categoryId));
+});
 
 export default component$(() => {
-  const name = useSignal('Qwik');
-  const capitalizedName = useComputed$(() => {
-    // it will automatically reexecute when name.value changes
-    return name.value.toUpperCase();
+  const products = useProductsLoader();
+  const isResultsEmpty = useComputed$(() => products.value.length === 0);
+
+  const { categorySelected } = useContext(ProductsContext);
+
+  useVisibleTask$(() => {
+    categorySelected.value = 2;
   });
 
-  console.log('render');
   return (
-    <>
-      <input type="text" bind:value={name} />
-      <p>Name: {name.value}</p>
-      <p>Capitalized name: {capitalizedName.value}</p>
-    </>
+    <main class="pt-10 lg:flex lg:h-screen lg:overflow-y-hidden">
+      <section class="lg:w-2/3 lg:h-screen lg:overflow-y-scroll py-24 px-10">
+        {isResultsEmpty.value ? (
+          <p class="text-center text-4xl">No hay productos</p>
+        ) : (
+          <div class="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-5">
+            {products.value.map((product) => (
+              <ProductCard product={product} key={product.id} />
+            ))}
+          </div>
+        )}
+      </section>
+      <aside class="lg:w-1/3 lg:h-screen lg:overflow-y-scroll pt-10 pb-32 px-10"></aside>
+    </main>
   );
 });
