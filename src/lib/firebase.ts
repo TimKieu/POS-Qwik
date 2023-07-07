7dbffbd00314384118217c5e56dec4ad5d9f2b4f
gustavocadev <gustavocadev@gmail.com>
2023-07-07 14:22:15 -0500

    chore: use server functions

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,3 +1,4 @@
+import { server$ } from '@builder.io/qwik-city';
 import { initializeApp } from 'firebase/app';
 import {
   getFirestore,
@@ -11,25 +12,28 @@ import { getStorage } from 'firebase/storage';
 import type { Product } from '~/types/Product';
 import type { Product as IProduct } from '~/types/Product';
 
-const firebaseConfig = {
+const firebaseConfig = server$(() => {
+  return {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-};
-
-export const firebaseApp = initializeApp(firebaseConfig);
+  appId: process.env.FIREBASE_APP_ID
+}
+})
 
 // My Server methods
+export const firebaseApp = server$(async () => {
+  return initializeApp(await firebaseConfig())
+})
 
-export const getFirebaseStorage = () => getStorage(firebaseApp)
+export const getFirebaseStorage = async () => getStorage(await firebaseApp())
 
-export const getDB = () => getFirestore(firebaseApp)
+export const getDB = async () => getFirestore(await firebaseApp())
 
-export const getCollection = async (q: Query<DocumentData>) => {
-  const productSnapshot = await getDocs(q);
+export const getCollection = async  (q: Query<DocumentData>) => {
+    const productSnapshot = await getDocs(q);
 
   const productList = productSnapshot.docs.map((doc) => ({
     ...doc.data(),
@@ -38,6 +42,7 @@ export const getCollection = async (q: Query<DocumentData>) => {
 
   return productList;
 }
+
 
 export const getDocument = async (docRef: DocumentReference<DocumentData>) => {
   const docSnapshot = await getDoc(docRef);
