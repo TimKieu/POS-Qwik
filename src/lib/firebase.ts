a604c8824bb9c5f7df8ff617b198acf02a4dac00
gustavocadev <gustavocadev@gmail.com>
2023-07-08 15:35:48 -0500

    chore: add process env

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -13,20 +13,20 @@ import type { Product } from '~/types/Product';
 import type { Product as IProduct } from '~/types/Product';
 
 const firebaseConfig =  {
-  apiKey: import.meta.env.PUBLIC_FIREBASE_API_KEY,
-  authDomain: import.meta.env.PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.PUBLIC_FIREBASE_APP_ID
+  apiKey: process.env['PUBLIC_FIREBASE_API_KEY'],
+  authDomain: process.env['PUBLIC_FIREBASE_AUTH_DOMAIN'],
+  projectId: process.env['PUBLIC_FIREBASE_PROJECT_ID'],
+  storageBucket: process.env['PUBLIC_FIREBASE_STORAGE_BUCKET'],
+  messagingSenderId: process.env['PUBLIC_FIREBASE_MESSAGING_SENDER_ID'],
+  appId: process.env['PUBLIC_FIREBASE_APP_ID']
 }
 
 // My Server methods
 export const firebaseApp = initializeApp(firebaseConfig)
 
-export const getFirebaseStorage = async () => getStorage(firebaseApp)
+export const getFirebaseStorage =  () => getStorage(firebaseApp)
 
-export const getDB = async () => getFirestore(firebaseApp)
+export const getDB =  () => getFirestore(firebaseApp)
 
 export const getCollection = async  (q: Query<DocumentData>) => {
     const productSnapshot = await getDocs(q);
