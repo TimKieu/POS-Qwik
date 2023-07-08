1aac6084e38801ccf416d858c1e365c2c3ffe8f7
gustavocadev <gustavocadev@gmail.com>
2023-07-08 15:36:29 -0500

    chore: rm Public prefix

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -13,12 +13,12 @@ import type { Product } from '~/types/Product';
 import type { Product as IProduct } from '~/types/Product';
 
 const firebaseConfig =  {
-  apiKey: process.env['PUBLIC_FIREBASE_API_KEY'],
-  authDomain: process.env['PUBLIC_FIREBASE_AUTH_DOMAIN'],
-  projectId: process.env['PUBLIC_FIREBASE_PROJECT_ID'],
-  storageBucket: process.env['PUBLIC_FIREBASE_STORAGE_BUCKET'],
-  messagingSenderId: process.env['PUBLIC_FIREBASE_MESSAGING_SENDER_ID'],
-  appId: process.env['PUBLIC_FIREBASE_APP_ID']
+  apiKey: process.env['FIREBASE_API_KEY'],
+  authDomain: process.env['FIREBASE_AUTH_DOMAIN'],
+  projectId: process.env['FIREBASE_PROJECT_ID'],
+  storageBucket: process.env['FIREBASE_STORAGE_BUCKET'],
+  messagingSenderId: process.env['FIREBASE_MESSAGING_SENDER_ID'],
+  appId: process.env['FIREBASE_APP_ID']
 }
 
 // My Server methods
