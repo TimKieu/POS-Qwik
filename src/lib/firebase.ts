d99f16aa144732809088bd5df8ae38e3ae5a99f0
gustavocadev <gustavocadev@gmail.com>
2023-07-08 15:26:12 -0500

    chore: use import meta env

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -13,12 +13,12 @@ import type { Product } from '~/types/Product';
 import type { Product as IProduct } from '~/types/Product';
 
 const firebaseConfig =  {
-  apiKey: process.env.PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.PUBLIC_FIREBASE_APP_ID
+  apiKey: import.meta.env.PUBLIC_FIREBASE_API_KEY,
+  authDomain: import.meta.env.PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.PUBLIC_FIREBASE_APP_ID
 }
 
 // My Server methods
